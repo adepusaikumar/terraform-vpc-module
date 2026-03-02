b87a84447de87d654610bb795803c0daddf78c09
saikumar001 <adepusaikumar0076@gmail.com>
2026-03-02 23:43:50 +0530

    terraform

--- main.tf
+++ main.tf
@@ -12,16 +12,47 @@ resource "aws_internet_gateway" "main" {
 
 # Public Subnets
 resource "aws_subnet" "public" {
-  vpc_id = aws_vpc.main.id
   count = length(var.public_subnet_cidrs)
+  vpc_id = aws_vpc.main.id
   cidr_block = var.public_subnet_cidrs[count.index]
+  availability_zone = local.az_names[count.index]
+  map_public_ip_on_launch = true
+  tags = merge(
+    local.common_tags,
+    {
+      # roboshop-dev-public-us-east-1a
+      Name = "${var.project}-${var.environment}-public-${local.az_names[count.index]}"
+    },
+    var.public_subnet_tags
+  )
+}
+
+# Private Subnets
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidrs)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidrs[count.index]
   availability_zone = local.az_names[count.index]
   tags = merge(
     local.common_tags,
     {
-      # roboshop-dev-public-
-      Name = "${var.project}-public-${var.environment}"
+      Name = "${var.project}-${var.environment}-private-${local.az_names[count.index]}"
     },
-    var.public_subnet_tags
+    var.private_subnet_tags
+  )
+}
+
+# Database Subnets
+resource "aws_subnet" "database" {
+  count = length(var.database_subnet_cidrs)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.database_subnet_cidrs[count.index]
+  availability_zone = local.az_names[count.index]
+  tags = merge(
+    local.common_tags,
+    {
+       Name = "${var.project}-${var.environment}-database-${local.az_names[count.index]}"
+    },
+    var.database_subnet_tags
   )
 }
